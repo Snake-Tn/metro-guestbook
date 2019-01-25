a0025d3bc2057d17ce4f35942a26f6d410a5b211
Ahmed Kooli <kooliahmd@gmail.com>
2019-01-25 22:09:34 +0100

    clean up CROSS headers

--- web/app.php
+++ web/app.php
@@ -6,16 +6,10 @@ $requestBuilder = new \Http\RequestBuilder();
 $request = $requestBuilder->build();
 
 $router = new Router(new Container);
-if ($request->getMethod() == "OPTIONS") {
-    header('Access-Control-Allow-Headers: authorization,content-type');
-    header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE');
-    header('Access-Control-Allow-Origin: *');
-    die;
-}
+
 $response = $router->route($request);
 
 header('Content-Type: ' . $response->contentType);
-header('Access-Control-Allow-Origin: *');
 
 http_response_code($response->code);
 echo $response->content;
